72f1c69bf48203639d8753f0b3d8dccca67fcca8
Andres <landres697@gmail.com>
2015-11-24 23:15:12 -0600

    login

--- whitelist.js
+++ whitelist.js
@@ -20,8 +20,8 @@
 
 if (!document.querySelector('meta[http-equiv=Content-Security-Policy]')) {
     var msg = 'No Content-Security-Policy meta tag found. Please add one when using the cordova-plugin-whitelist plugin.';
-    console.error(msg);
+    alert(msg);
     setInterval(function() {
-        console.warn(msg);
+        alert(msg);
     }, 10000);
 }
